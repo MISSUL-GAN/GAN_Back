1186a9dfb74b04837f1c65c71f8db44cd913625b
siyeonSon <siyeon0209@naver.com>
2022-07-22 23:05:11 +0900

    initial commit

---
+++ src/main/java/gan/missulgan/MissulganApplication.java
@@ -0,0 +1,13 @@
+package gan.missulgan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissulganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissulganApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gan/missulgan/MissulganApplicationTests.java
@@ -0,0 +1,13 @@
+package gan.missulgan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissulganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
