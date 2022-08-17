dca99eed1785dc0bec99cbbb58ec8bdf5c14916a
siyeonSon <siyeon0209@naver.com>
2022-08-17 21:22:17 +0900

    feat(login) : NAVER 로그인

--- src/main/java/gan/missulgan/security/oauth/dto/OAuth2UserAttributeFactory.java
+++ src/main/java/gan/missulgan/security/oauth/dto/OAuth2UserAttributeFactory.java
@@ -8,6 +8,7 @@ public class OAuth2UserAttributeFactory {
 
     private static final String KAKAO_REGISTRATION_ID = "kakao";
     private static final String GOOGLE_REGISTRATION_ID = "google";
+    private static final String NAVER_REGISTRATION_ID = "naver";
 
     public static OAuth2UserAttribute of(String registrationId, Map<String, Object> attributes) {
         if (registrationId.equals(KAKAO_REGISTRATION_ID)) {
@@ -15,6 +16,9 @@ public class OAuth2UserAttributeFactory {
         }
         if (registrationId.equals(GOOGLE_REGISTRATION_ID)) {
             return new GoogleOAuth2UserAttribute(attributes);
+        }
+        if (registrationId.equals(NAVER_REGISTRATION_ID)) {
+            return new NaverOAuth2UserAttribute(attributes);
         }
         throw new OAuth2AuthenticationException("PROVIDER_NOT_SUPPORTED: " + registrationId);
     }
