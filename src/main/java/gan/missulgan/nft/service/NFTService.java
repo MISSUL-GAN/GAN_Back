4e8342a0edc3c2faf97399a2981514b983c15fae
siyeonSon <siyeon0209@naver.com>
2022-08-23 11:23:53 +0900

    feat(NFT): NFT automatic minting

--- src/main/java/gan/missulgan/nft/service/NFTService.java
+++ src/main/java/gan/missulgan/nft/service/NFTService.java
@@ -23,34 +23,39 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class NFTService {
 
+    @Value("${app.nftStorageToken}")
+    private String NFT_STORAGE_ACCESS_TOKEN;
+
     @Value("${app.nftAuthorizationKey}")
     private static String NFT_AUTHORIZATION_KEY;
-    private static final String NFT_PORT_URL = "https://api.nftport.xyz/v0/metadata";
+
+    private static final String IMAGE_IPFS_URL = "https://api.nft.storage/upload";
     private static final String IPFS_PREFIX = "https://ipfs.io/ipfs/";
+    private static final String METADATA_IPFS_URL = "https://api.nftport.xyz/v0/metadata";
+    private static final String MINTING_URL = "https://api.nftport.xyz/v0/mints/customizable";
+
     private final RestTemplate restTemplate;
 
-    public Resource imagefileToIPFS(byte[] bytes, ImageType imageType) throws IOException {
+    public Resource imageToIPFS(byte[] bytes, ImageType imageType) throws IOException {
         HttpHeaders headers = new HttpHeaders();
         MediaType mediaType = MediaType.valueOf(imageType.getContentType());
         headers.setContentType(mediaType);
-        headers.set("Authorization", NFT_AUTHORIZATION_KEY);
-
+        headers.setBearerAuth(NFT_STORAGE_ACCESS_TOKEN);
         HttpEntity<byte[]> request = new HttpEntity<>(bytes, headers);
 
-        ResponseEntity<String> response =
-                restTemplate.postForEntity(NFT_PORT_URL, request, String.class);
-
+        ResponseEntity<String> response = restTemplate.postForEntity(IMAGE_IPFS_URL, request, String.class);
         if (response.getStatusCode().equals(HttpStatus.OK)) {
             String body = response.getBody();
             JsonParser jsonParser = new JsonParser();
             JsonObject jsonObject = (JsonObject) jsonParser.parse(body);
-            JsonPrimitive ipfs_url = jsonObject.getAsJsonPrimitive("ipfs_url");
-            return new UrlResource(IPFS_PREFIX + ipfs_url.getAsString());
+            jsonObject = jsonObject.getAsJsonObject("value");
+            JsonPrimitive cid = jsonObject.getAsJsonPrimitive("cid");
+            String ipfs = cid.getAsString();
+            return new UrlResource(IPFS_PREFIX + ipfs);
         }
         return null;
     }
 
-    // TODO : name/description(FROM Drawing), file_url(FROM Image)
     public String metadataToIPFS(String name, String description, String file_url) throws IOException {
         // Request Header
         HttpHeaders headers = new HttpHeaders();
@@ -68,7 +73,7 @@ public class NFTService {
 
         // Response
         ResponseEntity<String> response =
-                restTemplate.postForEntity(NFT_PORT_URL, request, String.class);
+                restTemplate.postForEntity(METADATA_IPFS_URL, request, String.class);
 
         if (response.getStatusCode().equals(HttpStatus.OK)) {
             String body = response.getBody();
@@ -76,6 +81,32 @@ public class NFTService {
             JsonObject jsonObject = (JsonObject) jsonParser.parse(body);
             JsonPrimitive metadata_uri = jsonObject.getAsJsonPrimitive("metadata_uri");
             return metadata_uri.getAsString();
+        }
+        return null;
+    }
+
+    public HttpStatus minting(String contract_address, String metadata_uri, String mint_to_address) throws IOException {
+        // Request Header
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", NFT_AUTHORIZATION_KEY);
+
+        // Request Body
+        JSONObject requestJson = new JSONObject();
+        requestJson.put("chain", "polygon");
+        requestJson.put("contract_address", contract_address);
+        requestJson.put("metadata_uri", metadata_uri);
+        requestJson.put("mint_to_address", mint_to_address);
+
+        // Request
+        HttpEntity<JSONObject> request = new HttpEntity<>(requestJson, headers);
+
+        // Response
+        ResponseEntity<String> response =
+                restTemplate.postForEntity(MINTING_URL, request, String.class);
+
+        if (response.getStatusCode().equals(HttpStatus.OK)) {
+            return HttpStatus.CREATED;
         }
         return null;
     }
--- src/test/java/gan/missulgan/nft/service/NFTServiceTest.java
+++ src/test/java/gan/missulgan/nft/service/NFTServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
@@ -22,10 +23,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class NFTServiceTest {
 
+    @Value("${app.nftStorageToken}")
+    private String token;
+
     @Value("${app.nftAuthorizationKey}")
     private String key;
 
     private RestTemplate restTemplate;
+
     @InjectMocks
     private NFTService nftService;
 
@@ -38,6 +43,7 @@ class NFTServiceTest {
                 .build();
         this.nftService = new NFTService(restTemplate);
         ReflectionTestUtils.setField(nftService, "NFT_AUTHORIZATION_KEY", key);
+        ReflectionTestUtils.setField(nftService, "NFT_STORAGE_ACCESS_TOKEN", token);
     }
 
     @Test
@@ -45,9 +51,9 @@ class NFTServiceTest {
     @DisplayName("이미지를 IPFS로 저장")
     void imageToIPFS() throws IOException {
         byte[] bufferExpected = {1, 1};
-        Resource resource = NFTService.imagefileToIPFS(bufferExpected, ImageType.PNG);
-        System.out.println("resource = " + resource.getURI());
-        assertThat(resource.getURI()).isNotNull();
+        Resource ipfs = nftService.imageToIPFS(bufferExpected, ImageType.PNG);
+        System.out.println("image_ipfs = " + ipfs);
+        assertThat(ipfs).isNotNull();
     }
 
     @Test
@@ -57,8 +63,19 @@ class NFTServiceTest {
         String name = "MissulGAN";
         String description = "MissulGAN LOGO";
         String file_url = "https://ipfs.io/ipfs/bafkreiapdwp65rgye2f7isg4bg55mneudaouuuaugctw2qms2aqzr3eeii";
-        String url = nftService.metadataToIPFS(name, description, file_url);
-        System.out.println("url = " + url);
-        assertThat(url).isNotBlank();
+        String ipfs = nftService.metadataToIPFS(name, description, file_url);
+        System.out.println("metadata_ipfs = " + ipfs);
+        assertThat(ipfs).isNotNull();
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("민팅")
+    void minting() throws IOException {
+        String contract_address = "0x6b3E6b7276EeB3Cd3A0E62BBC9f60351256b0988";
+        String metadata_uri = "ipfs://bafkreics74xcppy4lt4tsedhejhfldkli3qcy7jnvjrbkkihh4hahtvlxe";
+        String mint_to_address = "0x4e614dfb5b65705b3b7e48a1dac453362902596f";
+        HttpStatus status = nftService.minting(contract_address, metadata_uri, mint_to_address);
+        assertThat(status).isEqualTo(HttpStatus.CREATED);
     }
 }
